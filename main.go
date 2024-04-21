e86e5af6875300107bb908ddf3fb6cad5a1f3ebe
Munatul Awwaliyah <Munatul Awwaliyah@gmail.com>
2024-04-21 20:07:40 +0700

    code added

--- main.go
+++ main.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
+type Product interface {
+	DoStuff() string
+}
+
+type Food struct {
+	Name string
+}
+
+func (f *Food) DoStuff() string {
+	return f.Name
+}
+
+type NasiGoreng struct {
+	Food
+}
+
+type AyamGoreng struct {
+	Food
+}
+
+func CreateFood(name string) Product {
+	switch name {
+	case "Nasi Goreng":
+		return &NasiGoreng{
+			Food: Food{Name: "Nasi Goreng"},
+		}
+	case "Ayam Goreng":
+		return &AyamGoreng{
+			Food: Food{Name: "Ayam Goreng"},
+		}
+	default:
+		return nil
+	}
+}
 func main() {
-	fmt.Println("dddd")
+	nasiGoreng := CreateFood("Nasi Goreng")
+	fmt.Println(nasiGoreng.DoStuff())
+
+	ayamGoreng := CreateFood("Ayam Goreng")
+	fmt.Println(ayamGoreng.DoStuff())
 }
