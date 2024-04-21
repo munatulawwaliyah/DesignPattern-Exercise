60e75a86e5478d2d453c889c6fd8279977be28e3
Munatul Awwaliyah <Munatul Awwaliyah@gmail.com>
2024-04-21 20:26:11 +0700

    code update

--- main.go
+++ main.go
@@ -22,7 +22,23 @@ type AyamGoreng struct {
 	Food
 }
 
-func CreateFood(name string) Product {
+type Drink struct {
+	Name string
+}
+
+func (d *Drink) DoStuff() string {
+	return d.Name
+}
+
+type Americano struct {
+	Drink
+}
+
+type CaramelLatte struct {
+	Drink
+}
+
+func CreateProduct(name string) Product {
 	switch name {
 	case "Nasi Goreng":
 		return &NasiGoreng{
@@ -32,14 +48,28 @@ func CreateFood(name string) Product {
 		return &AyamGoreng{
 			Food: Food{Name: "Ayam Goreng"},
 		}
+	case "Americano":
+		return &Americano{
+			Drink: Drink{Name: "Americano"},
+		}
+	case "CaramelLatte":
+		return &CaramelLatte{
+			Drink: Drink{Name: "CaramelLatte"},
+		}
 	default:
 		return nil
 	}
 }
 func main() {
-	nasiGoreng := CreateFood("Nasi Goreng")
+	nasiGoreng := CreateProduct("Nasi Goreng")
 	fmt.Println(nasiGoreng.DoStuff())
 
-	ayamGoreng := CreateFood("Ayam Goreng")
+	ayamGoreng := CreateProduct("Ayam Goreng")
 	fmt.Println(ayamGoreng.DoStuff())
+
+	americano := CreateProduct("Americano")
+	fmt.Println(americano.DoStuff())
+
+	caramelLatte := CreateProduct("CaramelLatte")
+	fmt.Println(caramelLatte.DoStuff())
 }
